8afbc9018d443aa80bf1df84e4827a615483fcd7
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2024-02-26 16:07:52 +0100

    added new caster

--- tests/Casters/DateTimeCasterTest.php
+++ tests/Casters/DateTimeCasterTest.php
@@ -2,20 +2,23 @@
 
 namespace ACAT\Dto\Tests\Casters;
 
+use DateTimeImmutable;
 use ACAT\Dto\Tests\TestCase;
 use ACAT\Dto\DataTransferObject;
 use ACAT\Dto\Attributes\CastWith;
 use ACAT\Dto\Casters\DateTimeCaster;
 
+/**
+ *
+ */
 class DateTimeCasterTest extends TestCase {
 
     /**
      * @return void
      */
     public function testDateTimeCaster() : void {
-
         $bar = new Bar(date: '2024-01-01');
-
+        $this->assertInstanceOf(DateTimeImmutable::class, $bar->date);
     }
 
 }
@@ -26,6 +29,6 @@ class DateTimeCasterTest extends TestCase {
 class Bar extends DataTransferObject {
 
     #[CastWith(DateTimeCaster::class, "Y-m-d")]
-    public string $date;
+    public DateTimeImmutable $date;
 
 }
